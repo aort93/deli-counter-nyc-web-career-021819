5ea001e55f794b9d57c0feb0a6fb1610680f5feb
Alonso Ortega <alonsojortega@gmail.com>
2019-01-24 23:02:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,15 @@
 def line(line)
   if(line.size == 0)
     puts "The line is currently empty."
+  else
+    str = "The line is currently:"
+    idx = 0
+    line.each do |person|
+      idx += 1
+      str += " #{idx}. #{person}"
+    end
+
+    puts str
   end
 
 
